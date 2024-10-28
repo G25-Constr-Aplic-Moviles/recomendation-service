58d7752f2912546397491cdb4335ce357cb1fe09
Juan David Orduz Pinilla <j.orduz@uniandes.edu.co>
2024-10-28 13:11:29 -0500

    correciones

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,6 @@
 import firebase_admin
 from flask import Flask
-from firebase_admin import credentials,initialize_app
 
-cred = credentials.Certificate("api/key.json")
-default_app = initialize_app(cred)
 
 def create_app():
     app = Flask(__name__)
